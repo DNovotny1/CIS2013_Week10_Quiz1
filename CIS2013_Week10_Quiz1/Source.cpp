0562a8a717dadb952b7a2e745ab2832b3d56792b
Daniel Novotny <svc_okc_bt204@ad.osuokc.edu>
2018-03-28 19:06:06 -0500

    fixed for space

--- CIS2013_Week10_Quiz1/Source.cpp
+++ CIS2013_Week10_Quiz1/Source.cpp
@@ -9,10 +9,25 @@ int main() {
 
 	while (cont = 'y') {
 		int bCount = 1;
+		
 		cout << "\nEnter your string.\n";
-		cin >> input;
+		//cin >> input;
+		getline(cin, input);
+
+
+
+
+		//int size = input.size;
+		/*char arr[10];
 		for (int i = 0; i < input.size(); i++) {
-			if (input[i] == 'b') {
+			arr[i] = input[i];
+		}
+
+*/
+
+
+		for (int i = 0; i < input.size(); i++) {
+			if (input[i] == 'b' || input[i] == 'B') {
 				if (bCount % 2 == 0) {
 					cout << "STAND";
 					bCount++;
@@ -21,15 +36,15 @@ int main() {
 					cout << "SIT";
 					bCount++;
 				}
-
-
+			}
+			else if (input[i] == char(32)) {
+				cout << ' ';
 			}
 
 			else {
 				cout << input[i];
 			}
 		}
-
 	}
 	return 0;
 }
